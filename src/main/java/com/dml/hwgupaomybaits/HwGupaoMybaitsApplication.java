a6b3683f69aba6a00fed39fb92de48ba7bf0feff
louis <louis.liao@5aixi.com>
2019-06-25 10:55:10 +0800

    init

---
+++ src/main/java/com/dml/hwgupaomybaits/HwGupaoMybaitsApplication.java
@@ -0,0 +1,13 @@
+package com.dml.hwgupaomybaits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwGupaoMybaitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwGupaoMybaitsApplication.class, args);
+    }
+
+}
